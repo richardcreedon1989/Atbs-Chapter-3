555abb8beec32a4246d6f22e0bae94ab92ff27d1
Richard Creedon <richardcreedon1989@gmail.com>
2020-03-25 12:53:28 +0000

    updated

--- Chapter 3 project.py
+++ Chapter 3 project.py
@@ -1,16 +1,17 @@
 def collatz(number):
+    result = my_number
     if number % 2 == 0:
-        print (number // 2)
+        print(result)
         return number // 2
-    elif number % 2==1:
-        print (number *3 +1)
+    else:
+        print(result)
         return number *3 +1
 
-print('Enter number here')
-
-my_number=int(input())
+my_number=int(input('Enter a number:'))
 
 while collatz(my_number) != 1:
-    collatz(my_number)
+    my_number = collatz(my_number)
     if my_number == 1:
-        print(my_number)
+     print(my_number)
+
+
